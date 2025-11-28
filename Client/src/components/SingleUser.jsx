49aa39728ddf00078c2ca580ba99e2095465e9a7
Aditya <adityathakur00199.8@gmail.com>
2025-11-28 20:39:39 +0530

    donw with the svg

--- Client/src/components/SingleUser.jsx
+++ Client/src/components/SingleUser.jsx
@@ -78,11 +78,31 @@ function SingleUser() {
               <p><strong>GitHub ID:</strong> {user.githubUsername}</p>
             </div>
             <div className='flex gap-4'>
-              <button onClick={handleUpdate} className='bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded cursor-pointer transition-all duration-200 '>
-                Update
+              <button onClick={handleUpdate} className= ' flex gap-3 items-center bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded cursor-pointer transition-all duration-200 '>
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                strokeWidth="1.5" 
+                stroke="currentColor" 
+                className="size-6">
+                <path strokeLinecap="round" 
+                strokeLinejoin="round" 
+                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                </svg>
+                <p className='font-semibold'>Update</p>
               </button>
-              <button onClick={handleDelete} className='bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded cursor-pointer transition-all duration-200 '>
-                Delete
+              <button onClick={handleDelete} className='bg-red-500 hover:bg-red-600 flex items-center gap-3  text-white px-4 py-2 rounded cursor-pointer transition-all duration-200 '>
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                strokeWidth="1.5" 
+                stroke="currentColor" 
+                className="size-6">
+                <path strokeLinecap="round" 
+                strokeLinejoin="round" 
+                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                </svg>
+                <p className='font-semibold'>Delete</p>
               </button>
             </div>
           </>
