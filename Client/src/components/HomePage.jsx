9cc00b9b24e02ed42407b010cc110dcc56efc978
Aditya <adityathakur00199.8@gmail.com>
2025-12-11 12:52:53 +0530

    doneee

--- Client/src/components/HomePage.jsx
+++ Client/src/components/HomePage.jsx
@@ -12,9 +12,9 @@ function HomePage() {
         <div className='text-center text-yellow-300 '>
             <h1 className='text-6xl font-bold font-["Playfair_Display"] text-shadow-yellow-400 text-shadow-md'>Welcome to the GitHub Devs Cards</h1>
             <p className='mt-4 text-3xl text-shadow-yellow-400 text-shadow-sm'>Make beautiful and reuseable Github Dev Cards</p>
+            <p>this is a website to make github cards </p>
             <button onClick={() => navigate("/register")} className='mt-8 px-4 py-2 bg-yellow-400 text-black rounded-xl cursor-pointer hover:bg-yellow-500 text-xl transition-colors duration-300 shadow-sm shadow-yellow-300'>REGISTER YOURSELF</button>
         </div>
-        
     </div>
   )
 }
